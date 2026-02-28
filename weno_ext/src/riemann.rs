3e229b02eb646116ca22e351150aa5c85d2d16e4
colinhu123 <2877986200@qq.com>
2026-02-28 02:03:32 +0000

    implement HLLE and shock sensor

--- weno_ext/src/riemann.rs
+++ weno_ext/src/riemann.rs
@@ -1,4 +1,4 @@
-use ndarray::{ArrayView3,Array3};
+use ndarray::{ArrayView3,Array3, ArrayView2, Array2};
 use crate::utils;
 
 
@@ -193,6 +193,219 @@ pub(crate) fn hlle_x_local(q_l: ArrayView3<'_,f64>,
                             q_r: ArrayView3<'_,f64>,
                             f_r:ArrayView3<'_,f64>,
                             gamma:f64)
-{
+-> Array3<f64>{
+let (m, n, _c) = q_l.dim();
+    let mut out = Array3::<f64>::zeros((m, n, 4));
+    
 
+    for i in 0..m {
+        for j in 0..n {
+            // load
+            let ql0 = q_l[[i, j, 0]].max(utils::DEFAULT_EPS);
+            let ql1 = q_l[[i, j, 1]];
+            let ql2 = q_l[[i, j, 2]];
+            let ql3 = q_l[[i, j, 3]];
+            let qr0 = q_r[[i, j, 0]].max(utils::DEFAULT_EPS);
+            let qr1 = q_r[[i, j, 1]];
+            let qr2 = q_r[[i, j, 2]];
+            let qr3 = q_r[[i, j, 3]];
+
+            let u_l = ql1 / ql0;
+            let v_l = ql2 / ql0;
+            let u_r = qr1 / qr0;
+            let v_r = qr2 / qr0;
+            let p_l = (gamma - 1.0) * (ql3 - 0.5 * ql0 * (u_l * u_l + v_l * v_l));
+            let p_r = (gamma - 1.0) * (qr3 - 0.5 * qr0 * (u_r * u_r + v_r * v_r));
+            let p_l = p_l.max(utils::DEFAULT_EPS);
+            let p_r = p_r.max(utils::DEFAULT_EPS);
+            let h_l = (ql3 + p_l) / ql0;
+            let h_r = (qr3 + p_r) / qr0;
+
+            let sqrt_rho_l = ql0.sqrt();
+            let sqrt_rho_r = qr0.sqrt();    
+            let denom_r = sqrt_rho_l + sqrt_rho_r;
+            let u_tilde = (sqrt_rho_l * u_l + sqrt_rho_r * u_r) / denom_r;
+            let v_tilde = (sqrt_rho_l * v_l + sqrt_rho_r * v_r) / denom_r;
+            let h_tilde = (sqrt_rho_l * h_l + sqrt_rho_r * h_r) / denom_r;
+            let tmp = h_tilde - 0.5 * (u_tilde * u_tilde + v_tilde * v_tilde);
+            let a_tilde = ((gamma - 1.0) * tmp).max(0.0).sqrt();
+
+            let s_l = u_tilde - a_tilde;
+            let s_r = u_tilde + a_tilde;
+
+            // denom for Sstar
+            let mut denom = s_r - s_l;
+            if denom.abs() < utils::DEFAULT_EPS {
+                denom = utils::DEFAULT_EPS.copysign(denom) + utils::DEFAULT_EPS;
+            }
+
+
+            // pick flux
+            let fl = [f_l[[i, j, 0]], f_l[[i, j, 1]], f_l[[i, j, 2]], f_l[[i, j, 3]]];
+            let fr = [f_r[[i, j, 0]], f_r[[i, j, 1]], f_r[[i, j, 2]], f_r[[i, j, 3]]];
+
+            let mut fij = [0.0_f64; 4];
+
+            if s_l >= 0.0 {
+                fij = fl;
+            } else if s_r < 0.0 {
+                fij = fr;
+            } else {
+                //let denom = (s_r-s_l).max(utils::DEFAULT_EPS);
+                let q_l = [ql0, ql1, ql2, ql3];
+                let q_r = [qr0, qr1, qr2, qr3];
+                for k in 0..4 {
+                    fij[k] = s_r*fl[k] - s_l*fr[k] + s_r*s_l*(q_r[k] - q_l[k])/denom;
+                }
+                
+            }
+            for k in 0..4 {
+                out[[i,j,k]] = fij[k];
+            }
+            
+        }
+    }
+    out
 }
+
+pub(crate) fn hlle_y_local(q_l: ArrayView3<'_,f64>,
+                            f_l:ArrayView3<'_,f64>,
+                            q_r: ArrayView3<'_,f64>,
+                            f_r:ArrayView3<'_,f64>,
+                            gamma:f64)
+-> Array3<f64>{
+let (m, n, _c) = q_l.dim();
+    let mut out = Array3::<f64>::zeros((m, n, 4));
+    
+
+    for i in 0..m {
+        for j in 0..n {
+            // load
+            let ql0 = q_l[[i, j, 0]].max(utils::DEFAULT_EPS);
+            let ql1 = q_l[[i, j, 1]];
+            let ql2 = q_l[[i, j, 2]];
+            let ql3 = q_l[[i, j, 3]];
+            let qr0 = q_r[[i, j, 0]].max(utils::DEFAULT_EPS);
+            let qr1 = q_r[[i, j, 1]];
+            let qr2 = q_r[[i, j, 2]];
+            let qr3 = q_r[[i, j, 3]];
+
+            let u_l = ql1 / ql0;
+            let v_l = ql2 / ql0;
+            let u_r = qr1 / qr0;
+            let v_r = qr2 / qr0;
+            let p_l = (gamma - 1.0) * (ql3 - 0.5 * ql0 * (u_l * u_l + v_l * v_l));
+            let p_r = (gamma - 1.0) * (qr3 - 0.5 * qr0 * (u_r * u_r + v_r * v_r));
+            let p_l = p_l.max(utils::DEFAULT_EPS);
+            let p_r = p_r.max(utils::DEFAULT_EPS);
+            let h_l = (ql3 + p_l) / ql0;
+            let h_r = (qr3 + p_r) / qr0;
+
+            let sqrt_rho_l = ql0.sqrt();
+            let sqrt_rho_r = qr0.sqrt();    
+            let denom_r = sqrt_rho_l + sqrt_rho_r;
+            let u_tilde = (sqrt_rho_l * u_l + sqrt_rho_r * u_r) / denom_r;
+            let v_tilde = (sqrt_rho_l * v_l + sqrt_rho_r * v_r) / denom_r;
+            let h_tilde = (sqrt_rho_l * h_l + sqrt_rho_r * h_r) / denom_r;
+            let tmp = h_tilde - 0.5 * (u_tilde * u_tilde + v_tilde * v_tilde);
+            let a_tilde = ((gamma - 1.0) * tmp).max(0.0).sqrt();
+
+            let s_l = v_tilde - a_tilde;
+            let s_r = v_tilde + a_tilde;
+
+            // denom for Sstar
+            let mut denom = s_r - s_l;
+            if denom.abs() < utils::DEFAULT_EPS {
+                denom = utils::DEFAULT_EPS.copysign(denom) + utils::DEFAULT_EPS;
+            }
+
+
+            // pick flux
+            let fl = [f_l[[i, j, 0]], f_l[[i, j, 1]], f_l[[i, j, 2]], f_l[[i, j, 3]]];
+            let fr = [f_r[[i, j, 0]], f_r[[i, j, 1]], f_r[[i, j, 2]], f_r[[i, j, 3]]];
+
+            let mut fij = [0.0_f64; 4];
+
+            if s_l >= 0.0 {
+                fij = fl;
+            } else if s_r < 0.0 {
+                fij = fr;
+            } else {
+                //let denom = (s_r-s_l).max(utils::DEFAULT_EPS);
+                let q_l = [ql0, ql1, ql2, ql3];
+                let q_r = [qr0, qr1, qr2, qr3];
+                for k in 0..4 {
+                    fij[k] = s_r*fl[k] - s_l*fr[k] + s_r*s_l*(q_r[k] - q_l[k])/denom;
+                }
+                
+            }
+            for k in 0..4 {
+                out[[i,j,k]] = fij[k];
+            }
+            
+        }
+    }
+    out
+}
+
+
+pub(crate) fn shock_sensor_grad_p(p_input: ArrayView2<'_,f64>, jp_low: f64, jp_high: f64)
+-> Array2<f64>{
+    let (m,n) = p_input.dim();
+    let mut p = Array2::<f64>::zeros((m, n));
+    for i in 0..m {
+        for j in 0..n {
+            p[[i, j]] = p_input[[i,j]]
+        }
+    }
+
+    // spacing (you used 1/min_dim in the original)
+    let min_dim = (m.min(n)).max(1) as f64;
+    let dx = 1.0 / min_dim;
+
+    // derivatives
+    let mut dpdx = Array2::<f64>::zeros((m, n));
+    let mut dpdy = Array2::<f64>::zeros((m, n));
+
+    // interior finite differences (central)
+    for i in 1..(m-1) {
+        for j in 0..n {
+            dpdx[[i, j]] = (p[[i+1, j]] - p[[i-1, j]]) / (2.0 * dx);
+        }
+    }
+    // x-boundaries (one-sided)
+    for j in 0..n {
+        dpdx[[0, j]]       = (p[[1, j]] - p[[0, j]]) / (2.0 * dx);
+        dpdx[[m-1, j]] = (p[[m-1, j]] - p[[m-2, j]]) / (2.0 * dx);
+    }
+
+    // y-derivatives
+    for i in 0..m {
+        for j in 1..(n-1) {
+            dpdy[[i, j]] = (p[[i, j+1]] - p[[i, j-1]]) / (2.0 * dx);
+        }
+        // y-boundaries (one-sided)
+        dpdy[[i, 0]]       = (p[[i, 1]] - p[[i, 0]]) / (2.0 * dx);
+        dpdy[[i, n-1]] = (p[[i, n-1]] - p[[i, n-2]]) / (2.0 * dx);
+    }
+
+    // sensor phi (2D)
+    let mut phi = Array2::<f64>::zeros((m, n));
+    for i in 0..m {
+        for j in 0..n {
+            let grad = (dpdx[[i, j]].powi(2) + dpdy[[i, j]].powi(2)).sqrt();
+            let jp = grad / (p[[i, j]].max(utils::DEFAULT_EPS));
+            let w = if jp <= jp_low {
+                1.0
+            } else if jp >= jp_high {
+                0.0
+            } else {
+                1.0 - (jp - jp_low) / (jp_high - jp_low)
+            };
+            phi[[i, j]] = w;
+        }
+    }
+
+    phi
+}
+
